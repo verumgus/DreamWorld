8206ba5cf61affe8dd26158aeccc631248ccbf7f
Yan Hideki Yashiki <yan.yashiki@gmail.com>
2024-06-14 10:29:34 -0300

    Muitas coisas a fazer
    Melhorar UI , COMBATE E CENARIO

--- DreamWorld/Assets/Scripts/DialogueManager.cs
+++ DreamWorld/Assets/Scripts/DialogueManager.cs
@@ -1,21 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
 
-    public Queue sentences;
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> sentences;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
+        Debug.Log("Log baby" + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
         
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSetences();
     }
+
+    public void DisplayNextSetences()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string setence = sentences.Dequeue();
+        Debug.Log(setence);
+        dialogueText.text = setence;
+
+
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("Over");
+    }
+
 }
